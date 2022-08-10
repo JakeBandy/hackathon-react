bd7257a57193956cdcf2d57b7fab387ef894c433
Strevino2 <strevino2@hotmail.com>
2022-08-09 20:49:55 -0500

    functions working

---
+++ src/oldForLoop.js
@@ -0,0 +1,18 @@
+// for (let i = 0; i < data.hits.length; i++) {
+//     if (data.hits[i].url !== null) {
+//       const url = data.hits[i].url.search(input);
+//       if(data.hits[i].title !== null) {
+//         const title = data.hits[i].title.search(input);
+//         if(data.hits[i].author !== null){
+//           const author = data.hits[i].author.search(input);
+//         }
+//         if (url !== -1 || title !== -1 || author !== -1) {
+//           console.log("urlIndex ", url);
+//           results.push(data.hits[i]);
+//         }
+//         console.log("RESULTS ", results);
+//         setList(results);
+//       }
+
+//     }
+//   }
